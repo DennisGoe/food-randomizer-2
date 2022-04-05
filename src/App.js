a6a72a3fa46b7f03663faeee4f84d1ea1adf9287
DennisGoe <dennisgoericke@yahoo.de>
2022-04-05 13:51:58 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [list, setList] = useState([]);
+  const [chosenDish, setChosenDish] = useState("");
+  const [newDish, setNewDish] = useState("");
+
+  const removeItem = (index) => {
+    const listTemp = [...list];
+    listTemp.splice(index, 1);
+    setList(listTemp);
+  };
+  const chooseDish = () => {
+    setChosenDish(list[Math.floor(Math.random() * list.length)].name);
+  };
+  const addDish = () => {
+    console.log("triggered onclick without click");
+    if (newDish !== "") {
+      const newDishObject = {
+        name: newDish,
+        key: list.length,
+      };
+      list.push(newDishObject);
+      setNewDish("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div
+        style={{
+          display: "grid",
+          justifyContent: "center",
+        }}
+      >
+        <div>Enter dish: </div>
+        <input
+          type="text"
+          name="input"
+          value={newDish}
+          style={{ marginTop: "10px", marginBottom: "10px" }}
+          onChange={(event) => setNewDish(event.target.value)}
+        />
+        <button onClick={() => addDish()} style={{ marginBottom: "10px" }}>
+          Enter
+        </button>
+        {list.map((item, index) => {
+          return (
+            <div
+              key={item.key}
+              style={{
+                padding: "5px",
+                backgroundColor: "#f2f2f2",
+                boxShadow: "7px 7px 8px -1px rgba(0,0,0,0.34)",
+                marginBottom: "5px",
+                display: "flex",
+              }}
+            >
+              {item.name}
+              <div
+                key={item.key}
+                style={{
+                  border: "2px solid red",
+                  borderRadius: "5px",
+                  marginLeft: "3px",
+                  color: "red",
+                  paddingLeft: "5px",
+                  paddingRight: "5px",
+                  cursor: "pointer",
+                }}
+                onClick={() => removeItem(index)}
+              >
+                X
+              </div>
+            </div>
+          );
+        })}
+        <div
+          onClick={() => chooseDish()}
+          style={{
+            backgroundColor: "red",
+            color: "black",
+
+            padding: "5px",
+            display: "inline",
+            textAlign: "center",
+            boxShadow: "7px 7px 8px -1px rgba(0,0,0,0.34)",
+            cursor: "pointer",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          TELL ME WHAT TO COOK TONIGHT
+        </div>
+        <div
+          style={{
+            backgroundColor: "#7bfc03",
+            color: "black",
+            fontSize: "60px",
+            padding: "5px",
+            display: "inline",
+            textAlign: "center",
+            marginTop: "20px",
+            boxShadow: "7px 7px 8px -1px rgba(0,0,0,0.34)",
+            cursor: "pointer",
+          }}
+        >
+          {chosenDish}
+        </div>
+      </div>
     </div>
   );
 }
